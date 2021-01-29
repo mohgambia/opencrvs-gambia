a994e72e7dacbbfc9f4f0197a04381caa4d8cd78
Alfonso Tienda <afoone@hotmail.com>
2021-01-29 09:56:23 +0100

    restore metadata

---
+++ restore_metadata_manually.sh
@@ -0,0 +1,3 @@
+mongorestore --host localhost --drop --gzip --archive=./backups/openhim-dev.gz
+mongorestore --host localhost --drop --gzip --archive=./backups/hearth-dev.gz
+mongorestore --host localhost --drop --gzip --archive=./backups/user-mgnt.gz
